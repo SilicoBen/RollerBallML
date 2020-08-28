6d10a5a47e5d43868f5d2c5774352a8d72eb5e32
kbnealy <k.b.nealy@gmail.com>
2020-08-28 15:14:51 -0400

    Added Unity Project
    https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Learning-Environment-Create-New.md
    
    up to Testing the Environment

---
+++ Assets/RollerAgent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class RollerAgent : Agent
+{
+    Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public Transform Target;
+    public override void OnEpisodeBegin()
+    {
+        if (this.transform.localPosition.y < 0)
+        {
+            //If the agent fell, zero it's momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0,0.5f,0);
+        }
+        
+        // Move the target to a new spot
+        Target.localPosition = new Vector3(Random.value*8-4, 0.5f, Random.value*8-4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        //Target and agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+        
+        // Agent velocity 
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        //Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        
+        // Rewards 
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+        
+        // Reached Target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        
+        // Fell off platform
+        if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+}
